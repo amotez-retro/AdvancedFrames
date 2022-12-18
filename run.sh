2ca769d1d3a43e5a39240a712460a01245ba9b74
joshthibado <16496725+JoshThibado@users.noreply.github.com>
2022-12-18 16:16:24 -0700

    fixes

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # Generate frames from video with ffmpeg
 echo "Generate frames from video with ffmpeg"
-ffmpeg -ss 00:00:00 -to 00:30:00 -i $1 -r 10/1 -vf scale=240:160,setsar=1:1 -q:v 2 working/frames/output%03d.bmp
+ffmpeg -i $1 -nostdin -ss 00:00:00 -to 00:30:00 -r 10/1 -vf scale=240:160,setsar=1:1 -q:v 2 working/frames/output%03d.bmp
 
 # Run frames through rgb2gba and redirect to frames.h
 echo "Convert frames from bmp to header"
@@ -10,7 +10,7 @@ echo "Convert frames from bmp to header"
 
 # Generate pcm audio from input with ffmpeg
 echo "Generate pcm audio with ffmpeg"
-ffmpeg -y -ss 00:00:00 -to 00:30:00 -i $1 -acodec pcm_s8 -ac 1 -ar 8000 -f s8 working/audio.raw
+ffmpeg -i $1 -nostdin -y -ss 00:00:00 -to 00:30:00 -acodec pcm_s8 -ac 1 -ar 8000 -f s8 working/audio.raw
 
 # Create sound header with raw2gba
 echo "Convert pcm audio to header"
